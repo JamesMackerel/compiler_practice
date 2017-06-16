51e49adf77dce1f6e5e72dff816cd0e951b48a93
JAP <ghost.mackerel@gmail.com>
2017-06-16 11:09:39 +0800

    把Source类移动到单独的文件中。

---
+++ source.py
@@ -0,0 +1,55 @@
+from typing import *
+
+
+class Source:
+    """
+    represents a source file.
+    load all content to memory when initialize the object.
+    """
+
+    def __init__(self, file: str) -> None:
+        with open(file) as f:
+            self.source = f.read()
+
+        self.source += '\n'
+
+        self.i = 0  # index
+        self.r = 0  # row
+        self.c = 0  # column
+        self.last_c = 0
+
+    def get(self) -> Optional[str]:
+        """
+        get next character
+        :return: next character if exists, else returns None
+        """
+        try:
+            ch = self.source[self.i]
+        except IndexError:
+            return None
+
+        self.i += 1
+        self.c += 1
+
+        if ch == '\n':
+            self.last_c = self.c
+            self.r += 1
+            self.c = 0
+
+        return ch
+
+    def back(self) -> None:
+        self.i -= 1
+        if self.c == 0:
+            self.r -= 1
+            self.c = self.last_c
+        else:
+            self.c -= 1
+
+    def current_position(self) -> Tuple[int, int]:
+        """
+        get current position
+        :return: (row, column)
+        """
+        return self.r+1, self.c
+
