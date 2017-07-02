befd938f9c9491e4237071c857483225b7ab4ffd
JAP <ghost.mackerel@gmail.com>
2017-07-03 01:42:44 +0800

    改动测试源文件。

--- test.pas
+++ test.pas
@@ -1,20 +1,7 @@
-program test;
 begin
-  integer k;
-  integer function F(n);
-    begin
-      integer n;
-      if n<=0 then F:=1
-      else F:=n*F(n-1)
-    end;
-  integer function X(x);
-    begin
-        integer x;
-        if x>0 then x:=x-1
-        else x:=x-2
-      /* hell*o world */
-    end;
-  read(m);
-  k:=F(m);
-  write(k)
+  if x > 0 then x := x - 1
+  else x := x - 2;
+  {* hell * o world */*}
+  k:=F*m/2;
+  while x <> 0 do x := 15;
 end
