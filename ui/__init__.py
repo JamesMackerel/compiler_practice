7f604e9ba9e8f66bed6d4696296d0c746eefc9a5
JAP <ghost.mackerel@gmail.com>
2017-07-04 02:53:48 +0800

    修改了mainwindow对象的变量名。

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,9 +1,10 @@
 from .mainwindow import MainWindow
 from PyQt5.QtWidgets import QApplication
 
+
 def run():
     import sys
     app = QApplication(sys.argv)
-    mainwindow = MainWindow()
-    mainwindow.show()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
